f923543fc1b5a80846d1dba2bd0cd09be78ddcb2
Louise <andrei.ly4747@gmail.com>
2021-01-28 13:12:22 -0500

    plotting

--- try_kfold.py
+++ try_kfold.py
@@ -2,6 +2,8 @@ import pandas
 from sklearn import linear_model
 from sklearn.model_selection import KFold
 from sklearn import metrics
+from matplotlib import pyplot
+import numpy
 
 dataset = pandas.read_csv("dataset_3_outputs.csv")
 
@@ -13,6 +15,7 @@ kfold_object = KFold(n_splits = 4) #chop our dataset into 4 parts
 kfold_object.get_n_splits(data)
 
 #print(kfold_object)
+# k fold runs 4 time the original time if you run the same thing 4 times.
 i = 0
 for training_index, test_index in kfold_object.split(data):
 	print(i)
@@ -27,4 +30,12 @@ for training_index, test_index in kfold_object.split(data):
 	machine.fit(data_training, target_training)
 	new_target = machine.predict(data_test)
 	print("R2 score:", metrics.r2_score(target_test, new_target))
+
+	new_target_newaxis = new_target[:,numpy.newaxis]
+	results_machine = linear_model.LinearRegression()
+	results_machine.fit(new_target_newaxis,target_test)
+	pyplot.scatter(new_target_newaxis,target_test)
+	pyplot.plot(new_target_newaxis, results_machine.predict(new_target_newaxis), color='k')
+	pyplot.savefig('scatterplot' + str(i) +'.png')
+	#pyplot.clf()
 
